2b7ffa8d2fa9997cfa7d49611dcbbdcff5fa04c4
Gerry Paul <gpaul12@nc.rr.com>
2023-02-25 10:42:17 -0500

    js

--- Develop/script.js
+++ Develop/script.js
@@ -8,9 +8,9 @@ var time1 = beforeTime.add(0, "h");
 time1 = time1.format('hh:mm A');
 $(".block1").text(time1);
 
-var time2 = beforeTime.add(0, "h");
-time1 = time1.format('hh:mm A');
-$(".block1").text(time1);
+var time2 = beforeTime.add(1, "h");
+time2 = time2.format('hh:mm A');
+$(".block2").text(time2);
 
 var time1 = beforeTime.add(0, "h");
 time1 = time1.format('hh:mm A');
