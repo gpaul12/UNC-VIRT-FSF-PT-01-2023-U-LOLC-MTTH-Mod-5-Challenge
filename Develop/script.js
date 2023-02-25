d2fc4102780d4eb42361bca8c675318e70ad45f8
Gerry Paul <gpaul12@nc.rr.com>
2023-02-25 10:39:49 -0500

    time block

--- Develop/script.js
+++ Develop/script.js
@@ -3,3 +3,8 @@ $("#currentDay").text(moment().format('dddd MMMM DD YYYY, h:mm a'));
 var currentTime = moment();
 currentTime = currentTime.startOf("hour");
 var beforeTime = moment().startOf('day').add(9, "hours");
+
+var time1 = beforeTime.add(0, "h");
+time1 = time1.format('hh:mm A');
+$(".block1").text(time1);
+
